6797184a31457936ec0247c99e0e803f04f824af
SANTOSH MONDAL <santoshmondal@users.noreply.github.com>
2023-05-19 10:54:54 +0530

    addtodo.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import express from "express";
-import { readAllTodo } from "./db_todo.js";
+import { readAllTodo, insertTodo } from "./db_todo.js";
 
 const app = express();
 
+// localhost:4000/
 app.get("/", async (req, res) => {
   let list = await readAllTodo();
   res.json(list);
+});
+
+// localhost:4000/addtodo
+app.get("/addtodo", async (req, res) => {
+  await insertTodo({ message: "via api" });
+  res.json({ message: "Success" });
 });
 
 app.listen(4000);
